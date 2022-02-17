205d0ff532ef0d15e48cc4c75d347f16241fd1c8
Marco Roth <marco.roth@intergga.ch>
2022-02-17 09:15:33 +0100

    Setup StimulusReflex for Import Maps

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,12 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+import StimulusReflex from 'stimulus_reflex'
+import CableReady from 'cable_ready'
+
+import consumer from '../channels/consumer'
+import controller from '../controllers/application_controller'
+
+StimulusReflex.initialize(application, { consumer, controller, isolate: true })
+CableReady.initialize({ consumer })
